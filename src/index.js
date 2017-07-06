324e6b81281bd0df9ec7d6c596c2e0cc8bc29990
icapistrano <cc.igor@gmail.com>
2017-07-06 08:04:00 -0300

    new components add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+import { BrowserRouter, Match, Miss } from 'react-router';
+import StorePicker from './components/storepicker'
+import NotFound from './components/notfound'
+import App from './components/app'
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker}/>
+        <Match exactly pattern="/store/:storeId" component={App}/>
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'))
